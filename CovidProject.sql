060890471529097c5c3f6b5424ee0ede395cd5b4
Adam Buckley <145425461+AdamBuckley97@users.noreply.github.com>
2023-09-27 09:15:28 +0800

    Update CovidProject.sql

--- CovidProject.sql
+++ CovidProject.sql
@@ -1,6 +1,8 @@
 /*
 Covid 19 Data Exploration 
 
+Data can be downloaded here: https://ourworldindata.org/covid-deaths
+
 Skills used: Joins, CTE's, Temp Tables, Windows Functions, Aggregate Functions, Creating Views, Converting Data Types
 
 */
